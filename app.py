30bc4809bb2188d278112942f383ab2531d14af2
codecooker <mcodecooker@gmail.com>
2026-03-01 16:52:22 +0600

    decided to make blog

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/create')
+def create():
+    pass
+
+@app.route('/update')
+def update():
+    pass
+
+
+@app.route('/delete')
+def delete():
+    pass
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run()
